0b5acf3b2aa99913409ba0b248a4826c38fef028
azee <arjunrokaya@gmail.com>
2025-03-18 08:53:27 +0545

    car modified

--- src/com/model/Car.java
+++ src/com/model/Car.java
@@ -1,6 +1,24 @@
-public class Car{
+package com.model;
 
-   private String color;
-   private int price;
+public class Car {
+
+	private String color;
+	private int price;
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
 
 }
